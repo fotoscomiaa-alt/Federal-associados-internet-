612230824b4d345ddbd0e790f013ec2a39d6045c
fotoscomiaa-alt <fotoscomiaa@gmail.com>
2026-09-21 12:19:15 -0300

    Atualizar o Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,4 +4,4 @@ export default function Footer(){return <footer className="footer"><div classNam
  <div><h3>Empresa</h3><Link to="/sobre">Sobre nós</Link><Link to="/beneficios">Benefícios</Link><Link to="/faq">FAQ</Link></div>
  <div><h3>Planos</h3><Link to="/planos">Conheça os planos</Link><Link to="/contato">Contato</Link></div>
  <div><h3>Atendimento</h3><a href="https://wa.me/5511972506989">WhatsApp: 11 97250-6989</a></div>
- </div><div className="container copyright">© {new Date().getFullYear()} Federal Associados. Todos os direitos reservados.</div></footer>}
+ </div><div className="container copyright">© {new Date().getFullYear()} Federal Associados Internet. Todos os direitos reservados.</div></footer>}
